2f0522d7f16f239b86e0309dcc4749ef62dda775
Lam Le <lam.le@marahub.com>
2021-09-19 09:14:03 +0700

    updated: add tabs for printify and gearment

--- resources/views/order/printify.blade.php
+++ resources/views/order/printify.blade.php
@@ -20,18 +20,24 @@ $(document).ready(function(){
 
     // $('#myTab li:last-child a').tab('show')
 
+    // ///////////////////////////////////////////////////////
+    // PRINTIFY
+
     /**
      *
      */
-    $('select[name=blueprint_id]').on('change', function() {
-        updateProviderList([{id:0, title:'-- Loading --'}], false)
-        updateVariantsList([{id:0, title:'-- Loading --'}], false)
+    $('select[name$=blueprint_id].printify').on('change', function() {
+        var selectName = $(this).attr('name')
+        var itemId = selectName.split('_')[0]
+
+        updateProviderList(itemId, [{id:0, title:'-- Loading --'}], false)
+        updateVariantsList(itemId, [{id:0, title:'-- Loading --'}], false)
 
         $.post(
-            "{{url('/order/print-providers')}}",
+            "{{url('/print-providers/printify/providers')}}",
             {blueprint_id: this.value}
         ).done(function(res) {
-            updateProviderList(res.data, true)
+            updateProviderList(itemId, res.data, true)
         }).fail(function() {
             alert( "error" );
         });
@@ -40,12 +46,12 @@ $(document).ready(function(){
     /**
      *
      */
-    function updateProviderList(data, isTriggerChange) {
-        var $this = $('select[name=print_provider_id]').empty()
+    function updateProviderList(itemId, data, isTriggerChange) {
+        var $this = $(`select[name=${itemId}_print_provider_id].printify`).empty()
 
         if (data && data.length > 0) {
             data.forEach(element => {
-                $('select[name=print_provider_id]').append(`<option value="${element.id}">${element.title}</option>`)
+                $this.append(`<option value="${element.id}">${element.title}</option>`)
             });
         }
 
@@ -57,17 +63,20 @@ $(document).ready(function(){
     /**
      *
      */
-    $('select[name=print_provider_id]').on('change', function() {
-        updateVariantsList([{id:0, title:'-- Loading --'}], false)
+    $('select[name$=print_provider_id].printify').on('change', function() {
+        var selectName = $(this).attr('name')
+        var itemId = selectName.split('_')[0]
+
+        updateVariantsList(itemId, [{id:0, title:'-- Loading --'}], false)
 
         $.post(
-            "{{url('/order/variants')}}",
+            "{{url('/print-providers/printify/variants')}}",
             {
-                blueprint_id: $('select[name=blueprint_id]').val(),
+                blueprint_id: $(`select[name=${itemId}_blueprint_id].printify`).val(),
                 print_provider_id: this.value
             }
         ).done(function(res) {
-            updateVariantsList(res.data.variants, true)
+            updateVariantsList(itemId, res.data.variants, true)
         }).fail(function() {
             alert( "error" );
         });
@@ -76,31 +85,51 @@ $(document).ready(function(){
     /**
      *
      */
-    function updateVariantsList(data, isTriggerChange) {
-        var $this = $('select[name=variant_id]').empty()
+    function updateVariantsList(itemId, data, isTriggerChange) {
+        var $this = $(`select[name=${itemId}_variant_id].printify`).empty()
 
         if (data && data.length > 0) {
             data.forEach(element => {
-                $('select[name=variant_id]').append(`<option value="${element.id}">${element.title}</option>`)
+                $this.append(`<option value="${element.id}">${element.title}</option>`)
             });
         }
     }
+
+    // END PRINTIFY
+    // ///////////////////////////////////////////////////////
+
+
+
 });
 
 /**
  *
  */
-function submitOrderForm() {
-    console.log($('#orderForm').serializeArray())
-    var formdata = $('#orderForm').serializeArray().reduce((total, cur)=>{
-        total[cur.name] = cur.value
+function submitPrintifyForm() {
+    for (const element of $('#printifyForm').serializeArray()) {
+        if (element.value == 0) {
+            alert('Hãy chọn đầy đủ thông tin')
+            return;
+        }
+    }
+
+    var formdata = $('#printifyForm').serializeArray().reduce((total, cur) => {
+        var itemId = cur.name.split('_')[0]
+        var nameKey = cur.name.split('_').slice(1).join('_')
+
+        if (!total[itemId]) {
+            total[itemId] = {}
+            total[itemId]['order_id'] = itemId
+        }
+
+        if (!total[itemId][nameKey]) {
+            total[itemId][nameKey] = cur.value
+        }
+
         return total
     }, {})
-    console.log('formdata=', formdata)
 
-    if (formdata.print_provider_id == 0 || formdata.variant_id == 0) {
-        alert('Hãy chọn đầy đủ thông tin')
-    }
+    var processedFormdata = Object.entries(formdata).map(item => item[1])
 }
 
 </script>
@@ -113,55 +142,78 @@ function submitOrderForm() {
             <a class="nav-link" id="gearmentTab" data-toggle="tab" href="#gearment" role="tab" aria-controls="gearment" aria-selected="false">2. gearment</a>
         </li>
     </ul>
-    <form id='orderForm'>
-        <h3>Order Id: #{{$order->amz_order_id}}</h3>
-        <table class="table table-borderless table-hover">
-            @foreach($orderItems as $item)
-            <tr>
-                <td><img src="{{str_replace("._SCLZZZZZZZ__SX55_", "", $item->thumbnail)}}" alt="{{$item->product_name}}" class="img-thumbnail" style="max-width: 150px; max-height: 150px;"></td>
-                <td><h3 style="padding-top:10px">{{$item->product_name}} ({{$item->style}}; {{$item->size}}; {{$item->color}})</h3></td>
-            </tr>
-            <tr class="table-warning">
-                <td colspan="2">
-                    <div class="row">
-                        <div class="col">
-                            <label class="text-danger">Styles</label>
-                            <select class="custom-select" name='blueprint_id'>
-                                <option value="6">Unisex T-Shirt - 5000</option>
-                                <option value="466">Women's T-Shirt - 5000L</option>
-                            </select>
-                        </div>
-                        <div class="col">
-                            <label class="text-danger">Print Provider</label>
-                            <select class="custom-select" name='print_provider_id'>
-                                <option selected>Open this select menu</option>
-                                <option value="1">One</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
-                            </select>
-                        </div>
-                        <div class="col">
-                            <label class="text-danger">Variants of a Style</label>
-                            <select class="custom-select js-example-basic-single" name="variant_id">
-                                <option value="1">One</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
-                            </select>
-                        </div>
-                        <div class="col">
-                            <label class="text-danger">Design</label>
-                            <input type="text" class="form-control" placeholder="123456">
-                        </div>
+    <div class="tab-content">
+        <div class="tab-pane active" id="printify" role="tabpanel" aria-labelledby="printify-tab">
+            <form id='printifyForm'>
+                <h3>Order Id: #{{$order->amz_order_id}}</h3>
+                <table class="table table-borderless table-hover">
+                    @foreach($orderItems as $item)
+                    <tr>
+                        <td><img src="{{str_replace("._SCLZZZZZZZ__SX55_", "", $item->thumbnail)}}" alt="{{$item->product_name}}" class="img-thumbnail" style="max-width: 150px; max-height: 150px;"></td>
+                        <td><h3 style="padding-top:10px">{{$item->product_name}} ({{$item->style}}; {{$item->size}}; {{$item->color}})</h3></td>
+                    </tr>
+                    <tr class="table-warning">
+                        <td colspan="2">
+                            <div class="row">
+                                <div class="col">
+                                    <label class="text-danger">Styles</label>
+                                    <select class="custom-select printify" name="{{$item->id}}_blueprint_id">
+                                        <option value="6">Unisex T-Shirt - 5000</option>
+                                        <option value="466">Women's T-Shirt - 5000L</option>
+                                    </select>
+                                </div>
+                                <div class="col">
+                                    <label class="text-danger">Print Provider</label>
+                                    <select class="custom-select printify" name="{{$item->id}}_print_provider_id">
+                                        <option selected>Open this select menu</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </select>
+                                </div>
+                                <div class="col">
+                                    <label class="text-danger">Variants of a Style</label>
+                                    <select class="custom-select printify js-example-basic-single" name="{{$item->id}}_variant_id">
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </select>
+                                </div>
+                                <div class="col">
+                                    <label class="text-danger">Design</label>
+                                    <input type="text" class="form-control" placeholder="123456">
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    @endforeach
+                </table>
+                <div class="row" style="padding-top:20px">
+                    <div class="col">
+                        <button type="button" class="btn btn-primary col-12" onclick="submitPrintifyForm()">Submit Printify Order</button>
                     </div>
-                </td>
-            </tr>
-            @endforeach
-        </table>
-        <div class="row" style="padding-top:20px">
-            <div class="col">
-                <button type="button" class="btn btn-primary col-12" onclick="submitOrderForm()">Submit Order</button>
-            </div>
+                </div>
+            </form>
         </div>
-    </form>
+        <div class="tab-pane" id="gearment" role="tabpanel" aria-labelledby="gearment-tab">
+            <form id='gearmentForm'>
+                <h3>Order Id: #{{$order->amz_order_id}}</h3>
+                <table class="table table-borderless table-hover">
+                    @foreach($orderItems as $item)
+                    <tr>
+                        <td><img src="{{str_replace("._SCLZZZZZZZ__SX55_", "", $item->thumbnail)}}" alt="{{$item->product_name}}" class="img-thumbnail" style="max-width: 150px; max-height: 150px;"></td>
+                        <td><h3 style="padding-top:10px">{{$item->product_name}} ({{$item->style}}; {{$item->size}}; {{$item->color}})</h3></td>
+                    </tr>
+                    @endforeach
+                </table>
+                <div class="row" style="padding-top:20px">
+                    <div class="col">
+                        <button type="button" class="btn btn-primary col-12" onclick="submitGearmentForm()">Submit Gearment Order</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
 </div>
 @endsection
