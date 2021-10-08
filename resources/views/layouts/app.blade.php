d9825c0c99e58800d25053805a7e5fa14bb20e9a
phubk <maixphu@gmail.com>
2021-10-08 20:34:35 +0700

    menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -70,6 +70,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{ url('etsy/statistic') }}">statistics</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('blacklist') }}">blacklist</a>
+                        </li>
                         @endif
                         <!--@if(Auth::user()->isAdmin())-->
                         <!--<li class="nav-item dropdown">-->
